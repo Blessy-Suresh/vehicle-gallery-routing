3de7f26abff46c2906ddb476bb1bf07e4d843a83
Blessy Mol Suresh <blessymol268@gmail.com>
2026-02-23 10:42:11 +0530

    Delete src/component directory

--- src/component/cars.js
+++
@@ -1,5 +0,0 @@
-const cars=[{name:"audi",color:"Red",url:"https://images.pexels.com/photos/31575923/pexels-photo-31575923.jpeg"},
-    {name:"audi",color:"White",url:"https://images.pexels.com/photos/18502403/pexels-photo-18502403.jpeg"},
-    {name:"audi",color:"Black",url:"https://images.pexels.com/photos/27833024/pexels-photo-27833024.jpeg"},
-];
- export default cars;
